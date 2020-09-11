779366994cbce466ddf5cbd7720db0689d55f757
alexander baumann <baumannalexj@users.noreply.github.com>
2020-09-11 17:38:39 -0500

    Logging (#1)
    * added logging
    
    * moved logging config from xml to spring properties

--- application/src/main/java/application/ElevatorApplication.java
+++ application/src/main/java/application/ElevatorApplication.java
@@ -1,5 +1,9 @@
 package application;
 
+import application.model.Elevator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- application/src/main/java/application/config/ApplicationContext.java
+++ application/src/main/java/application/config/ApplicationContext.java
@@ -1,9 +1,26 @@
 package application.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
 
 @Configuration
 public class ApplicationContext {
+    private static final Logger LOG = LoggerFactory.getLogger(ApplicationContext.class);
+
+    @Value("${server.port}")
+    private int port;
+
+    @Value("${spring.application.name}")
+    private String name;
+
+    @PostConstruct
+    public void start() {
+        LOG.info("running \"" + name + "\" at http://localhost:" + port);
+    }
 
 
 }
